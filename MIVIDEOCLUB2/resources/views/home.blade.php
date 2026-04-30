2d64c55fb1cc4fea25b20cd329e4c422e9204146
SoutoSid7 <mariojesussoutocolon@gmail.com>
2026-04-30 10:36:05 +0200

    Lavarel

--- MIVIDEOCLUB2/resources/views/home.blade.php
+++ MIVIDEOCLUB2/resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Pantalla Principal') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Estas logeado') }}
 
                     <br><br>
                     <a href="{{ url('catalog') }}" class="btn btn-primary">
