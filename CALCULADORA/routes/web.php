e8ffac776b2c19c973c1c2bd5a89261e39a11d47
SoutoSid7 <mariojesussoutocolon@gmail.com>
2026-05-11 10:09:30 +0200

    Calculadora de Joel

--- CALCULADORA/routes/web.php
+++ CALCULADORA/routes/web.php
@@ -2,9 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CalculadoraController;
+//Sesion
+use App\Http\Controllers\SesionController;
 
-// get cuando entra por primera vez
-Route::get('/', [CalculadoraController::class, 'index'])->name('calculadora.index'); // da nombre especifico a la ruta te llama a la funcion
+Route::get('/',           [CalculadoraController::class, 'index']    )->name('calculadora');
+Route::post('/digito',    [CalculadoraController::class, 'digito']   )->name('digito');
+Route::post('/operacion', [CalculadoraController::class, 'operacion'])->name('operacion');
+Route::post('/calcular',  [CalculadoraController::class, 'calcular'] )->name('calcular');
+Route::post('/unaria',    [CalculadoraController::class, 'unaria']   )->name('unaria');
+Route::post('/activar',   [CalculadoraController::class, 'activar']  )->name('activar');
+Route::post('/borrar',    [CalculadoraController::class, 'borrar']   )->name('borrar');
+Route::post('/limpiar',   [CalculadoraController::class, 'limpiar']  )->name('limpiar');
 
-// post cada vez que el usuario aprieta un boton
-Route::post('/', [CalculadoraController::class, 'calcular'])->name('calculadora.calcular');
+// //Sesion
+// Route::get('/sesion', [SesionController::class, 'index']);
+// Route::post('/sesion/increment', [SesionController::class, 'increment']);
+// Route::post('/sesion/decrement', [SesionController::class, 'decrement']);
+// Route::post('/sesion/reset', [SesionController::class, 'reset']);
